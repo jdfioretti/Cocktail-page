f6655ceec2d6ded6a224fc4d25297b1c1d5453a5
jdfioretti <jdfioretti@gmail.com>
2022-11-28 19:26:00 -0300

    1st commit cocktail

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>Este es el primer proyecto de React con Next.js, que hacemos con "Los Tanos del Código", con la intención de practicar y tomar impuslo para otros proyectos, que nos ayuden a conseguir nuestros objetivos</p>
+    </section>
+  );
+}
+
+export default About
